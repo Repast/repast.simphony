e4352da75af586d6b28886182b9b9e7a0325d5b9
Nick Collier <srcnick@users.sourceforge.net>
2011-12-01 15:10:34 -0500

    deleted data.gui package

--- repast.simphony.data.ui/src/repast/simphony/data/gui/MappingWizardOption.java
+++
@@ -1,8 +0,0 @@
-/*CopyrightHere*/
-package repast.simphony.data.gui;
-
-import repast.simphony.util.wizard.WizardOption;
-
-public interface MappingWizardOption extends WizardOption {
-	void setIsAggregate(boolean isAggregate);
-}
