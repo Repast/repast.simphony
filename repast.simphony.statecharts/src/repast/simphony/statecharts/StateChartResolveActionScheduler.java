5b74b2997d59e41ca6e4901ea57d2223e66f59f5
Jonathan Ozik <xio247@gmail.com>
2012-11-16 15:56:36 -0600

    Removed bottleneck from StateChartResolveActionScheduler.

--- repast.simphony.statecharts/src/repast/simphony/statecharts/StateChartResolveActionScheduler.java
+++ repast.simphony.statecharts/src/repast/simphony/statecharts/StateChartResolveActionScheduler.java
@@ -11,62 +11,73 @@ import repast.simphony.engine.schedule.ISchedule;
 import repast.simphony.engine.schedule.ScheduleParameters;
 
 public enum StateChartResolveActionScheduler {
-	
+
 	INSTANCE;
-	
+
+	private final static long MAX_BEFOFE_CLEAR = 10000;
 	protected Map<Double, ResolveActionsMapValue> resolveActions = new HashMap<Double, ResolveActionsMapValue>();
 
 	static class ResolveActionsMapValue {
 		private StateChartResolveAction scra;
 		private ISchedulableAction isa;
 		private boolean remove = false;
-		
+
 		public ResolveActionsMapValue(StateChartResolveAction scra,
 				ISchedulableAction isa) {
 			this.scra = scra;
 			this.isa = isa;
 		}
-		
-		public void registerListener(StateChart sc){
+
+		public void registerListener(StateChart sc) {
 			scra.registerListener(sc);
 		}
-		
-		public void removeListener(StateChart sc){
+
+		public void removeListener(StateChart sc) {
 			scra.removeListener(sc);
-			if (!scra.hasListeners()){
-				RunEnvironment.getInstance().getCurrentSchedule().removeAction(isa);
+			if (!scra.hasListeners()) {
+				RunEnvironment.getInstance().getCurrentSchedule()
+						.removeAction(isa);
 				isa = null;
 				scra = null;
 				remove = true;
 			}
 		}
-		
-		public boolean toRemove(){
+
+		public boolean toRemove() {
 			return remove;
 		}
 	}
-	
-	public void initialize(){
-		for(Double key : resolveActions.keySet()) {
+
+	public void initialize() {
+		clearCounter = 0;
+		for (Double key : resolveActions.keySet()) {
 			ResolveActionsMapValue ramv = resolveActions.remove(key);
 			ramv.isa = null;
 			ramv.scra = null;
 		}
 	}
-	
-	public void clearOldResolveActions(){
-		double time = RunEnvironment.getInstance().getCurrentSchedule().getTickCount();
-		List<Double> keysToRemove = new ArrayList<Double>();
-		for(Double rTime: resolveActions.keySet()){
-			if (rTime.compareTo(time) <= 0) keysToRemove.add(rTime);
-		}
-		for(Double key : keysToRemove) {
-			ResolveActionsMapValue ramv = resolveActions.remove(key);
-			ramv.isa = null;
-			ramv.scra = null;
+
+	long clearCounter = 0;
+
+	public void clearOldResolveActions() {
+		clearCounter++;
+		if (clearCounter > MAX_BEFOFE_CLEAR) {
+			double time = RunEnvironment.getInstance().getCurrentSchedule()
+					.getTickCount();
+			List<Double> keysToRemove = new ArrayList<Double>();
+			for (Double rTime : resolveActions.keySet()) {
+				if (rTime.compareTo(time) <= 0)
+					keysToRemove.add(rTime);
+			}
+			for (Double key : keysToRemove) {
+				ResolveActionsMapValue ramv = resolveActions.remove(key);
+				ramv.isa = null;
+				ramv.scra = null;
+			}
+			clearCounter = 0;
 		}
 	}
-	
+
 	public void scheduleResolveTime(double nextTime, StateChart sc) {
 		ResolveActionsMapValue ramv = resolveActions.get(nextTime);
 		if (ramv == null) {
@@ -74,24 +85,26 @@ public enum StateChartResolveActionScheduler {
 					.getCurrentSchedule();
 			StateChartResolveAction scra = new StateChartResolveAction();
 			ISchedulableAction ia = schedule.schedule(ScheduleParameters
-					.createOneTime(nextTime, ScheduleParameters.LAST_PRIORITY),scra);
-			 ramv = new ResolveActionsMapValue(scra, ia);
+					.createOneTime(nextTime, ScheduleParameters.LAST_PRIORITY),
+					scra);
+			ramv = new ResolveActionsMapValue(scra, ia);
 			resolveActions.put(nextTime, ramv);
-			
+
 		}
 		ramv.registerListener(sc);
 	}
-	
+
 	public void removeResolveTime(double nextTime, StateChart sc) {
-		if (resolveActions.containsKey(nextTime)){
+		if (resolveActions.containsKey(nextTime)) {
 			ResolveActionsMapValue ramv = resolveActions.get(nextTime);
 			ramv.removeListener(sc);
-			if(ramv.toRemove()) resolveActions.remove(nextTime);
-		}
-		else {
-			throw new IllegalStateException("Excess removeResolveTime call detected for StateChart: " + sc);
+			if (ramv.toRemove())
+				resolveActions.remove(nextTime);
+		} else {
+			throw new IllegalStateException(
+					"Excess removeResolveTime call detected for StateChart: "
+							+ sc);
 		}
 	}
-	
-	
+
 }
