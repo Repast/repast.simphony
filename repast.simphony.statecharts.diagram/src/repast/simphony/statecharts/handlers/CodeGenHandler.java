0fd57aa565dac4af2d7e15336e8010437f4ae8f4
Nick Collier <srcnick@users.sourceforge.net>
2012-12-12 14:28:47 -0500

    added builder and menu handler to generate the code from the diagram.

---
+++ repast.simphony.statecharts.diagram/src/repast/simphony/statecharts/handlers/CodeGenHandler.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package repast.simphony.statecharts.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import repast.simphony.statecharts.generator.CodeGenerator;
+
+/**
+ * @author Nick Collier
+ */
+public class CodeGenHandler extends AbstractHandler {
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
+   * ExecutionEvent)
+   */
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    ISelection selection = HandlerUtil.getCurrentSelection(event);
+
+    if (selection instanceof IStructuredSelection) {
+      IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+      Object firstElement = structuredSelection.getFirstElement();
+      
+      if (firstElement instanceof IFile) {
+        IFile file = (IFile) firstElement;
+        
+        IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(file.getFullPath().segment(0));
+        IPath path = project.getLocation().append(file.getFullPath().removeFirstSegments(1));
+        
+        if (path.getFileExtension().equals(StatechartBuilder.STATECHART_EXTENSION)) {
+          try {
+            new CodeGenerator().run(project, path, null);
+          } catch (CoreException e) {
+            throw new ExecutionException("Error executing generate code handler", e);
+          }
+        }
+      }
+    }
+
+    return null;
+  }
+}
---
+++ repast.simphony.statecharts.diagram/src/repast/simphony/statecharts/handlers/StatechartBuilder.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package repast.simphony.statecharts.handlers;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import repast.simphony.statecharts.generator.CodeGenerator;
+
+/**
+ * @author Nick Collier
+ */
+public class StatechartBuilder extends IncrementalProjectBuilder {
+
+  public static final String STATECHART_EXTENSION = "rsc";
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.core.resources.IncrementalProjectBuilder#build(int,
+   * java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
+   */
+  @Override
+  protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor)
+      throws CoreException {
+    if (kind == IncrementalProjectBuilder.FULL_BUILD) {
+      fullBuild(monitor);
+    } else {
+      IResourceDelta delta = getDelta(getProject());
+      if (delta == null) {
+        fullBuild(monitor);
+      } else {
+        incrementalBuild(delta, monitor);
+      }
+    }
+    return null;
+  }
+
+  private void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor)
+      throws CoreException {
+    delta.accept(new Visitor(getProject(), monitor));
+  }
+
+  private void fullBuild(IProgressMonitor monitor) {
+    // TODO get all the rsc files in the project and build them.
+  }
+
+  private static class Visitor implements IResourceDeltaVisitor {
+
+    IProgressMonitor monitor;
+    IProject project;
+
+    public Visitor(IProject project, IProgressMonitor monitor) {
+      this.monitor = monitor;
+      this.project = project;
+    }
+
+    @Override
+    public boolean visit(IResourceDelta delta) throws CoreException {
+      IPath path = delta.getResource().getRawLocation();
+      System.out.println("statechart builder running: " + delta);
+      if (delta.getKind() == IResourceDelta.CHANGED && path != null
+          && path.getFileExtension() != null
+          && path.getFileExtension().equals(STATECHART_EXTENSION)) {
+        new CodeGenerator().run(project, path, monitor);
+      }
+      return true;
+    }
+  }
+}
