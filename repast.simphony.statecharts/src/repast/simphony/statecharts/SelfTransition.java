5c088d25b8f7fbe151e8279672dba1daa96cf12f
Nick Collier <srcnick@users.sourceforge.net>
2012-12-17 14:12:20 -0500

    added id parameter.

--- repast.simphony.statecharts/src/repast/simphony/statecharts/SelfTransition.java
+++ repast.simphony.statecharts/src/repast/simphony/statecharts/SelfTransition.java
@@ -1,9 +1,13 @@
 package repast.simphony.statecharts;
 
-public class SelfTransition<T> extends Transition<T>{
+public class SelfTransition<T> extends Transition<T> {
 
-	protected SelfTransition(Trigger trigger, AbstractState<T> state, double priority){
-		super(trigger,state,state,priority);
-	}
+  protected SelfTransition(Trigger trigger, AbstractState<T> state, double priority) {
+    super(trigger, state, state, priority);
+  }
+
+  protected SelfTransition(String id, Trigger trigger, AbstractState<T> state, double priority) {
+    super(id, trigger, state, state, priority);
+  }
 
 }
