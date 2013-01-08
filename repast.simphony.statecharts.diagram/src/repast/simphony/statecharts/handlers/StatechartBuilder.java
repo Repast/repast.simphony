75a2363ab4b52a9a0870ea545684161d41102305
Nick Collier <srcnick@users.sourceforge.net>
2013-01-08 14:17:58 -0500

    Added full building and better error message

--- repast.simphony.statecharts.diagram/src/repast/simphony/statecharts/handlers/StatechartBuilder.java
+++ repast.simphony.statecharts.diagram/src/repast/simphony/statecharts/handlers/StatechartBuilder.java
@@ -6,14 +6,19 @@ package repast.simphony.statecharts.handlers;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceStatus;
+import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
 
 import repast.simphony.statecharts.generator.CodeGenerator;
+import repast.simphony.statecharts.part.StatechartDiagramEditorPlugin;
 
 /**
  * @author Nick Collier
@@ -31,6 +36,7 @@ public class StatechartBuilder extends IncrementalProjectBuilder {
   @Override
   protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor)
       throws CoreException {
+    try {
     if (kind == IncrementalProjectBuilder.FULL_BUILD) {
       fullBuild(monitor);
     } else {
@@ -41,6 +47,10 @@ public class StatechartBuilder extends IncrementalProjectBuilder {
         incrementalBuild(delta, monitor);
       }
     }
+    } catch (CoreException ex) {
+      throw new CoreException(new Status(IResourceStatus.BUILD_FAILED, StatechartDiagramEditorPlugin.ID,
+          ex.getLocalizedMessage(), ex));
+    }
     return null;
   }
 
@@ -49,8 +59,34 @@ public class StatechartBuilder extends IncrementalProjectBuilder {
     delta.accept(new Visitor(getProject(), monitor));
   }
 
-  private void fullBuild(IProgressMonitor monitor) {
-    // TODO get all the rsc files in the project and build them.
+  private void fullBuild(IProgressMonitor monitor) throws CoreException {
+    getProject().accept(new FullBuildVisitor(getProject(), monitor));
+  }
+  
+  private static class FullBuildVisitor implements IResourceVisitor {
+    
+    IProgressMonitor monitor;
+    IProject project;
+    CodeGenerator generator;
+
+    public FullBuildVisitor(IProject project, IProgressMonitor monitor) {
+      this.monitor = monitor;
+      this.project = project;
+      generator = new CodeGenerator();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.core.resources.IResourceVisitor#visit(org.eclipse.core.resources.IResource)
+     */
+    @Override
+    public boolean visit(IResource resource) throws CoreException {
+      IPath path = resource.getRawLocation();
+      if (path != null && path.getFileExtension() != null && path.getFileExtension().equals(STATECHART_EXTENSION)) {
+        generator.run(project, path, monitor);
+      }
+      return true;
+    }
+    
   }
 
   private static class Visitor implements IResourceDeltaVisitor {
@@ -66,7 +102,7 @@ public class StatechartBuilder extends IncrementalProjectBuilder {
     @Override
     public boolean visit(IResourceDelta delta) throws CoreException {
       IPath path = delta.getResource().getRawLocation();
-      System.out.println("statechart builder running: " + delta);
+      //System.out.println("statechart builder running: " + delta);
       if ((delta.getKind() == IResourceDelta.CHANGED || delta.getKind() == IResourceDelta.ADDED) && path != null
           && path.getFileExtension() != null
           && path.getFileExtension().equals(STATECHART_EXTENSION)) {
