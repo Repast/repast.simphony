d13b21ddd0ed31dd0e9f3b8eb2b77a3aa1fa72dd
Nick Collier <srcnick@users.sourceforge.net>
2013-02-01 16:54:54 -0500

    added class name and package attributes.

--- repast.simphony.systemdynamics.diagram/src/repast/simphony/systemdynamics/sheets/SystemModelSheet.java
+++ repast.simphony.systemdynamics.diagram/src/repast/simphony/systemdynamics/sheets/SystemModelSheet.java
@@ -29,79 +29,133 @@ public class SystemModelSheet extends Composite {
   private Text txtStart;
   private Text txtEnd;
   private Text txtStep, txtInterval;
+  private Text txtClassName;
+  private Text txtPackage;
 
   public SystemModelSheet(FormToolkit toolkit, Composite parent) {
     super(parent, SWT.H_SCROLL);
     toolkit.adapt(this);
     toolkit.paintBordersFor(this);
-    setLayout(new GridLayout(2, false));
-    
-    Label lblStartTime = new Label(this, SWT.NONE);
-    lblStartTime.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+    GridLayout gridLayout = new GridLayout(2, false);
+    setLayout(gridLayout);
+
+    Composite composite = toolkit.createComposite(this, SWT.NONE);
+    composite.setLayout(new GridLayout(2, false));
+    composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+    toolkit.paintBordersFor(composite);
+
+    toolkit.createLabel(composite, "Model Properties", SWT.NONE);
+
+    Label lblFoo = new Label(composite, SWT.SEPARATOR | SWT.HORIZONTAL);
+    lblFoo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+    toolkit.adapt(lblFoo, true, true);
+
+    Composite composite_2 = toolkit.createComposite(this, SWT.NONE);
+    GridLayout gl_composite_2 = new GridLayout(2, false);
+    gl_composite_2.marginLeft = 6;
+    composite_2.setLayout(gl_composite_2);
+    composite_2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+    toolkit.paintBordersFor(composite_2);
+
+    Label lblStartTime = new Label(composite_2, SWT.NONE);
+    lblStartTime.setSize(63, 14);
     toolkit.adapt(lblStartTime, true, true);
     lblStartTime.setText("Start Time:");
-    
-    txtStart = new Text(this, SWT.BORDER);
+
+    txtStart = new Text(composite_2, SWT.BORDER);
     txtStart.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+    txtStart.setSize(325, 19);
     toolkit.adapt(txtStart, true, true);
     txtStart.addVerifyListener(new DoubleVerifier());
     Bug383650Fix.applyFix(txtStart);
-    
-    Label lblEndTime = new Label(this, SWT.NONE);
-    lblEndTime.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+    Label lblEndTime = new Label(composite_2, SWT.NONE);
+    lblEndTime.setSize(58, 14);
     toolkit.adapt(lblEndTime, true, true);
     lblEndTime.setText("End Time:");
-    
-    txtEnd = new Text(this, SWT.BORDER);
-    txtEnd.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+    txtEnd = new Text(composite_2, SWT.BORDER);
+    txtEnd.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+    txtEnd.setSize(325, 19);
     toolkit.adapt(txtEnd, true, true);
     txtEnd.addVerifyListener(new DoubleVerifier());
     Bug383650Fix.applyFix(txtEnd);
-    
-    Label lblTimeStep = new Label(this, SWT.NONE);
-    lblTimeStep.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+    Label lblTimeStep = new Label(composite_2, SWT.NONE);
+    lblTimeStep.setSize(110, 14);
     toolkit.adapt(lblTimeStep, true, true);
     lblTimeStep.setText("Integration Interval:");
-    
-    txtStep = new Text(this, SWT.BORDER);
-    txtStep.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+    txtStep = new Text(composite_2, SWT.BORDER);
+    txtStep.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     toolkit.adapt(txtStep, true, true);
     txtStep.addVerifyListener(new DoubleVerifier());
     Bug383650Fix.applyFix(txtStep);
-    
-    Label lblInterval = new Label(this, SWT.NONE);
-    lblInterval.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+    Label lblInterval = new Label(composite_2, SWT.NONE);
+    lblInterval.setSize(103, 14);
     toolkit.adapt(lblInterval, true, true);
     lblInterval.setText("Reporting Interval:");
-    
-    txtInterval = new Text(this, SWT.BORDER);
-    txtInterval.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+    txtInterval = new Text(composite_2, SWT.BORDER);
+    txtInterval.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     toolkit.adapt(txtInterval, true, true);
     txtInterval.addVerifyListener(new DoubleVerifier());
     Bug383650Fix.applyFix(txtInterval);
-    
-    Label lblUnits = new Label(this, SWT.NONE);
-    lblUnits.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+    Label lblUnits = new Label(composite_2, SWT.NONE);
+    lblUnits.setSize(35, 14);
     toolkit.adapt(lblUnits, true, true);
     lblUnits.setText("Units:");
-    
-    cmbUnits = new Combo(this, SWT.NONE);
-    GridData gd_cmbUnits = new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1);
-    gd_cmbUnits.widthHint = 200;
-    cmbUnits.setLayoutData(gd_cmbUnits);
+
+    cmbUnits = new Combo(composite_2, SWT.NONE);
+    cmbUnits.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     toolkit.adapt(cmbUnits);
     toolkit.paintBordersFor(cmbUnits);
+
+    Composite composite_1 = toolkit.createComposite(this, SWT.NONE);
+    composite_1.setLayout(new GridLayout(2, false));
+    composite_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+    toolkit.paintBordersFor(composite_1);
+
+    toolkit.createLabel(composite_1, "Code Properties", SWT.NONE);
+
+    Label label = new Label(composite_1, SWT.SEPARATOR | SWT.HORIZONTAL);
+    label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+    toolkit.adapt(label, true, true);
+    new Label(this, SWT.NONE);
+
+    Composite composite_3 = toolkit.createComposite(this, SWT.NONE);
+    composite_3.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+    toolkit.paintBordersFor(composite_3);
+    composite_3.setLayout(new GridLayout(2, false));
+
+    Label lblClassName = toolkit.createLabel(composite_3, "Class Name:", SWT.NONE);
+    lblClassName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+    txtClassName = toolkit.createText(composite_3, "New Text", SWT.NONE);
+    txtClassName.setText("");
+    txtClassName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+    Bug383650Fix.applyFix(txtClassName);
+
+    Label lblPackage = toolkit.createLabel(composite_3, "Package:", SWT.NONE);
+    lblPackage.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+    txtPackage = toolkit.createText(composite_3, "New Text", SWT.NONE);
+    txtPackage.setText("");
+    txtPackage.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
+    Bug383650Fix.applyFix(txtPackage);
   }
-  
+
   protected void bind(EMFDataBindingContext context, EObject eObject, EAttribute attribute,
       Widget widget) {
     context.bindValue(
         WidgetProperties.text(new int[] { SWT.Modify }).observeDelayed(400, widget),
         EMFEditProperties.value(TransactionUtil.getEditingDomain(eObject), attribute).observe(
             eObject), createUpdateValueStrategy(new StringToDoubleConverter()),
-            createUpdateValueStrategy(new DoubleToStringConverter()));
+        createUpdateValueStrategy(new DoubleToStringConverter()));
   }
-  
+
   protected UpdateValueStrategy createUpdateValueStrategy(IConverter converter) {
     UpdateValueStrategy strategy = new UpdateValueStrategy();
     strategy.setConverter(converter);
@@ -112,15 +166,17 @@ public class SystemModelSheet extends Composite {
     java.util.List<String> allUnits = SDModelUtils.getAllUnits(eObject);
     Collections.sort(allUnits);
     cmbUnits.setItems(allUnits.toArray(new String[0]));
-    
+
     context.bindValue(
         WidgetProperties.text().observe(cmbUnits),
         EMFEditProperties.value(TransactionUtil.getEditingDomain(eObject),
             SDModelPackage.Literals.SYSTEM_MODEL__UNITS).observe(eObject));
-    
+
     bind(context, eObject, SDModelPackage.Literals.SYSTEM_MODEL__START_TIME, txtStart);
     bind(context, eObject, SDModelPackage.Literals.SYSTEM_MODEL__END_TIME, txtEnd);
     bind(context, eObject, SDModelPackage.Literals.SYSTEM_MODEL__TIME_STEP, txtStep);
     bind(context, eObject, SDModelPackage.Literals.SYSTEM_MODEL__REPORTING_INTERVAL, txtInterval);
+    bind(context, eObject, SDModelPackage.Literals.SYSTEM_MODEL__CLASS_NAME, txtClassName);
+    bind(context, eObject, SDModelPackage.Literals.SYSTEM_MODEL__PACKAGE, txtPackage);
   }
 }
