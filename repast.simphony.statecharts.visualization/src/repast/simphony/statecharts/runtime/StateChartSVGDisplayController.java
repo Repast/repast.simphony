792cd38261cfcf82a43a0bd2fd7f5fa27ecc37fd
Nick Collier <srcnick@users.sourceforge.net>
2013-03-05 16:20:18 -0500

    fixed issue on when space in path on windows.

--- repast.simphony.statecharts.visualization/src/repast/simphony/statecharts/runtime/StateChartSVGDisplayController.java
+++ repast.simphony.statecharts.visualization/src/repast/simphony/statecharts/runtime/StateChartSVGDisplayController.java
@@ -1,5 +1,6 @@
 package repast.simphony.statecharts.runtime;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -16,122 +17,111 @@ import repast.simphony.statecharts.StateChartListener;
 
 public class StateChartSVGDisplayController implements StateChartListener {
 
-	private StateChart stateChart;
-	StateChartSVGDisplay svgDisplay;
-	StateChartSVGModel model;
-	
+  private StateChart stateChart;
+  StateChartSVGDisplay svgDisplay;
+  StateChartSVGModel model;
 
-	Object agent;
+  Object agent;
 
-	public StateChartSVGDisplayController(Object agent, StateChart stateChart) {
-		this.agent = agent;
-		this.stateChart = stateChart;
-	}
+  public StateChartSVGDisplayController(Object agent, StateChart stateChart) {
+    this.agent = agent;
+    this.stateChart = stateChart;
+  }
 
-	private String createFrameTitle() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Agent: ");
-		sb.append(agent);
-		sb.append(", ");
-		sb.append("Statechart: ");
-		sb.append(stateChart.getClass().getName());
-		return sb.toString();
-	}
+  private String createFrameTitle() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Agent: ");
+    sb.append(agent);
+    sb.append(", ");
+    sb.append("Statechart: ");
+    sb.append(stateChart.getClass().getName());
+    return sb.toString();
+  }
 
-	protected URI locateSVGResource() {
-		StringBuilder sb = new StringBuilder();
-		sb.append('/');
-		String path = stateChart.getClass().getName().replace('.', '/');
-		sb.append(path);
-		sb.append(".svg");
-		URL resource = this.getClass().getResource(sb.toString());
-		if (resource != null) {
-			try {
-				return resource.toURI();
-			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return null;
-	}
+  protected URI locateSVGResource() {
+    StringBuilder sb = new StringBuilder();
+    sb.append('/');
+    String path = stateChart.getClass().getName().replace('.', '/');
+    sb.append(path);
+    sb.append(".svg");
+    URL resource = stateChart.getClass().getResource(sb.toString());
+    if (resource != null) {
+      return new File(resource.getFile()).toURI();
+    }
+    return null;
+  }
 
-	public void createAndShowDisplay() {
-		final String frameTitle = createFrameTitle();
-		final URI uri = locateSVGResource();
-		if (uri != null) {
-			javax.swing.SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					initializeDisplay(frameTitle, uri);
-				}
-			});
-		} else {
-			System.out.println("SVG resource not located.");
-		}
-	}
+  public void createAndShowDisplay() {
+    final String frameTitle = createFrameTitle();
+    final URI uri = locateSVGResource();
+    if (uri != null) {
+      javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          initializeDisplay(frameTitle, uri);
+        }
+      });
+    } else {
+      System.out.println("SVG resource not located.");
+    }
+  }
 
-	/**
-	 * Initialize display.
-	 * Should be called from EDT.
-	 * @param frameTitle
-	 * @param uri
-	 */
-	private void initializeDisplay(String frameTitle, URI uri) {
-		svgDisplay = new StateChartSVGDisplay(this,frameTitle, uri);
-		svgDisplay.initialize();
-	}
-	
-	
+  /**
+   * Initialize display. Should be called from EDT.
+   * 
+   * @param frameTitle
+   * @param uri
+   */
+  private void initializeDisplay(String frameTitle, URI uri) {
+    svgDisplay = new StateChartSVGDisplay(this, frameTitle, uri);
+    svgDisplay.initialize();
+  }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
 
-		
+  }
 
-	}
+  @Override
+  public void update() {
+    List<AbstractState> states = stateChart.getCurrentStates();
+    Set<String> activeUUIDs = new LinkedHashSet<String>();
+    for (AbstractState state : states) {
+      activeUUIDs.add(stateChart.getUuidForState(state));
+    }
+    model.setActiveUUIDs(activeUUIDs);
+    svgDisplay.renewDocument();
+  }
 
-	@Override
-	public void update() {
-		List<AbstractState> states = stateChart.getCurrentStates();
-		Set<String> activeUUIDs = new LinkedHashSet<String>();
-		for (AbstractState state : states){
-			activeUUIDs.add(stateChart.getUuidForState(state));
-		}
-		model.setActiveUUIDs(activeUUIDs);
-		svgDisplay.renewDocument();
-	}
-	
-	// Called from display code after the svg document is loaded by the JSVGCanvas
-	public void initializeModel(SVGDocument svgDoc) {
-		model = new StateChartSVGModel(svgDoc);
-		svgDisplay.setModel(model);
-		stateChart.registerStateChartListener(this);
-	}
-	
-	private List<StatechartCloseListener> scls = new ArrayList<StatechartCloseListener>();
-	
-	public void registerCloseListener(StatechartCloseListener scl){
-		scls.add(scl);
-	}
+  // Called from display code after the svg document is loaded by the JSVGCanvas
+  public void initializeModel(SVGDocument svgDoc) {
+    model = new StateChartSVGModel(svgDoc);
+    svgDisplay.setModel(model);
+    stateChart.registerStateChartListener(this);
+  }
 
-	public void notifyCloseListeners() {
-		for (StatechartCloseListener scl : scls){
-			scl.statechartClosed();
-		}
-	}
+  private List<StatechartCloseListener> scls = new ArrayList<StatechartCloseListener>();
 
+  public void registerCloseListener(StatechartCloseListener scl) {
+    scls.add(scl);
+  }
 
-	public void closeDisplayWithoutNotification(){
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				svgDisplay.closeFrame();
-			}
-		});
-		
-	}
+  public void notifyCloseListeners() {
+    for (StatechartCloseListener scl : scls) {
+      scl.statechartClosed();
+    }
+  }
+
+  public void closeDisplayWithoutNotification() {
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+      @Override
+      public void run() {
+        svgDisplay.closeFrame();
+      }
+    });
+
+  }
 
 }
