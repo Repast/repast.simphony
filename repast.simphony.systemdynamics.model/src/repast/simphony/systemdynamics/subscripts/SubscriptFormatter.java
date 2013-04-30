149d8be92baaad974cb35f1f9acb83e63088a277
Nick Collier <srcnick@users.sourceforge.net>
2013-04-30 17:30:55 -0400

    redid subscript adding to add the subscript at the cursor position regardless and have some smarts re. brackets.

---
+++ repast.simphony.systemdynamics.model/src/repast/simphony/systemdynamics/subscripts/SubscriptFormatter.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package repast.simphony.systemdynamics.subscripts;
+
+/**
+ * @author Nick Collier
+ */
+public class SubscriptFormatter {
+  
+  private String eq = "";
+  
+  public SubscriptFormatter(String equation) {
+    this.eq = equation;
+  }
+ 
+  /**
+   * formats the specified subscript for adding at the specified location.
+   * 
+   * @param loc
+   * @param subscript
+   */
+  public String format(int loc, String subscript) {
+    
+    String left = eq.substring(0, loc);
+    String right = eq.substring(loc, eq.length());
+    
+    // is loc inside brackets.
+    boolean inBrackets = false;
+    int leftBIndex = left.lastIndexOf("[");
+    if (leftBIndex != -1 && leftBIndex > left.lastIndexOf("]")) {
+      int rightBIndex = right.indexOf("]");
+      int index = right.indexOf("[");
+      inBrackets = rightBIndex != -1 && (rightBIndex < index || index == -1);
+    }
+    
+    if (inBrackets) {
+      return subscript;
+    } else {
+      return "[" + subscript + "]";
+    }
+  }
+}
