2e42108ddd2ebd4f877a9b272e267c8a65f46618
Nick Collier <srcnick@users.sourceforge.net>
2013-05-01 13:52:12 -0400

    classpath variable for running stand alone batch via a launcher.

---
+++ repast.simphony.eclipse/src/repast/simphony/eclipse/StandAloneBatchCPInit.java
@@ -0,0 +1,43 @@
+package repast.simphony.eclipse;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.ClasspathVariableInitializer;
+import org.eclipse.jdt.core.JavaCore;
+import org.osgi.framework.Bundle;
+
+/**
+ * Initializes the StandAloneBatch classpath variable. This is used to launch
+ * the stand alone batch GUI from eclipse itself.
+ * 
+ * @author Nick Collier
+ */
+public class StandAloneBatchCPInit extends ClasspathVariableInitializer {
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.eclipse.jdt.core.ClasspathVariableInitializer#initialize(java.lang.
+   * String)
+   */
+  @Override
+  public void initialize(String var) {
+    Bundle bundle = Platform.getBundle("repast.simphony.distributed.batch.ui");
+    if (bundle != null) {
+      URL url = FileLocator.find(bundle, new Path("bin-standalone"), null);
+      try {
+        URI uri = FileLocator.resolve(url).toURI();
+        JavaCore.setClasspathVariable(var, new Path(new File(uri).getAbsolutePath()), null);
+      } catch (Exception ex) {
+        RepastSimphonyPlugin.getInstance().log(ex);
+      }
+
+    }
+  }
+}
