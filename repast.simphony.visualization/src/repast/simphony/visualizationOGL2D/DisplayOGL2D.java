8286f0dfab12dc0f9b278bbe6eb8c0e462e97897
Nick Collier <srcnick@users.sourceforge.net>
2013-05-01 17:27:53 -0400

    added debug statements

--- repast.simphony.visualization/src/repast/simphony/visualizationOGL2D/DisplayOGL2D.java
+++ repast.simphony.visualization/src/repast/simphony/visualizationOGL2D/DisplayOGL2D.java
@@ -388,9 +388,11 @@ public class DisplayOGL2D extends AbstractDisplay implements CanvasListener, Pic
    * @see repast.simphony.visualization.IDisplay#update()
    */
   public void update() {
+    System.out.println("DisplayOGL2D.update 1");
     if (glInitialized && !iconified) {
       layoutUpdater.update();
       try {
+        System.out.println("DisplayOGL2D.update 2");
         canvas.getRenderLock().lock();
 
         for (ValueLayerDisplayLayer layer : valueLayerStyleMap.values()) {
@@ -409,6 +411,7 @@ public class DisplayOGL2D extends AbstractDisplay implements CanvasListener, Pic
         canvas.getRenderLock().unlock();
       }
     }
+    System.out.println("DisplayOGL2D.update finished");
   }
 
   /*
@@ -580,6 +583,7 @@ public class DisplayOGL2D extends AbstractDisplay implements CanvasListener, Pic
    * float, float, saf.ui.v3d.VNode)
    */
   public void reshape(GLAutoDrawable drawable, float width, float height, VRoot root) {
+    System.out.println("DisplayOGL2D.reshape");
     glInitialized = true;
     update();
     // System.out.printf("width: %f, height: %f%n", width, height);
