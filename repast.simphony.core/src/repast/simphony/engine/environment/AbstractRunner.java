f1a06ac1c35bfe952947991f36b2735250d00eb9
Nick Collier <srcnick@users.sourceforge.net>
2013-05-07 16:07:56 -0400

    added removeRunListener method.

--- repast.simphony.core/src/repast/simphony/engine/environment/AbstractRunner.java
+++ repast.simphony.core/src/repast/simphony/engine/environment/AbstractRunner.java
@@ -5,122 +5,136 @@ import java.util.List;
 
 import repast.simphony.engine.controller.Controller;
 
-
 /**
  * @author Nick Collier
  */
 public abstract class AbstractRunner implements Runner {
 
-	protected boolean stop = false;
+  protected boolean stop = false;
 
-	protected boolean pause = false;
+  protected boolean pause = false;
 
-	protected boolean step = false;
+  protected boolean step = false;
 
-	protected ArrayList<RunListener> runListeners = new ArrayList<RunListener>();
-	
-	protected Thread thread;
-	
-	protected RunEnvironmentBuilder environmentBuilder;
-	
-	protected Controller controller;
-	/**
-	 * Adds the specified listener to the list of RunListener-s to be notified of any run related
-	 * events, such as stopped, started, and so on.
-	 * 
-	 * @param listener
-	 *            the listener to add.
-	 */
-	public void addRunListener(RunListener listener) {
-		synchronized (runListeners) {
-			runListeners.add(listener);
-		}
-	}
+  protected ArrayList<RunListener> runListeners = new ArrayList<RunListener>();
 
-	/**
-	 * Fires a started message to all registered run listeners.
-	 */
-	protected void fireStartedMessage() {
-		List<RunListener> list = cloneListenerList();
-		for (RunListener listener : list) {
-			listener.started();
-		}
-	}
+  protected Thread thread;
 
-	/**
-	 * Fires a stopped message to all registered run listeners.
-	 */
-	protected void fireStoppedMessage() {
-		List<RunListener> list = cloneListenerList();
-		for (RunListener listener : list) {
-			listener.stopped();
-		}
-	}
-	
-	public void setEnvironmentBuilder(RunEnvironmentBuilder environment){
-		this.environmentBuilder=environment;
-	}
+  protected RunEnvironmentBuilder environmentBuilder;
 
-	/**
-	 * Fires a paused message to all registered run listeners.
-	 */
-	protected void firePausedMessage() {
-		List<RunListener> list = cloneListenerList();
-		for (RunListener listener : list) {
-			listener.paused();
-		}
-	}
+  protected Controller controller;
 
-	/**
-	 * Fires a restarted message to all registered run listeners.
-	 */
-	protected void fireRestartedMessage() {
-		List<RunListener> list = cloneListenerList();
-		for (RunListener listener : list) {
-			listener.restarted();
-		}
-	}
+  /**
+   * Adds the specified listener to the list of RunListener-s to be notified of
+   * any run related events, such as stopped, started, and so on.
+   * 
+   * @param listener
+   *          the listener to add.
+   */
+  public void addRunListener(RunListener listener) {
+    synchronized (runListeners) {
+      runListeners.add(listener);
+    }
+  }
 
-	@SuppressWarnings("unchecked")
-	private List<RunListener> cloneListenerList() {
-		List<RunListener> list = null;
-		synchronized (runListeners) {
-			list = (List<RunListener>) runListeners.clone();
-		}
-		return list;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * repast.simphony.engine.environment.Runner#removeRunListener(repast.simphony
+   * .engine.environment.RunListener)
+   */
+  @Override
+  public void removeRunListener(RunListener listener) {
+    synchronized (runListeners) {
+      runListeners.remove(listener);
+    }
 
-	public void init() {
-		stop = false;
-		pause = false;
-		step = false;
-	}
+  }
 
-	/**
-	 * Stops the execution of the scheduled events after the any events scheduled for the current
-	 * tick have been executed.
-	 */
-	public void stop() {
-		stop = true;
-	}
+  /**
+   * Fires a started message to all registered run listeners.
+   */
+  protected void fireStartedMessage() {
+    List<RunListener> list = cloneListenerList();
+    for (RunListener listener : list) {
+      listener.started();
+    }
+  }
 
-	public void step() {
-		step = true;
-	}
+  /**
+   * Fires a stopped message to all registered run listeners.
+   */
+  protected void fireStoppedMessage() {
+    List<RunListener> list = cloneListenerList();
+    for (RunListener listener : list) {
+      listener.stopped();
+    }
+  }
 
-	public void setPause(boolean pause) {
-		this.pause = pause;
-	}
-	
+  public void setEnvironmentBuilder(RunEnvironmentBuilder environment) {
+    this.environmentBuilder = environment;
+  }
 
-	/**
-	 * @return true if the runner should keep going
-	 */
-	public boolean go() {
-		return !stop;
-	}
-	
-	public void setController(Controller controller){
-		this.controller=controller;
-	}
+  /**
+   * Fires a paused message to all registered run listeners.
+   */
+  protected void firePausedMessage() {
+    List<RunListener> list = cloneListenerList();
+    for (RunListener listener : list) {
+      listener.paused();
+    }
+  }
+
+  /**
+   * Fires a restarted message to all registered run listeners.
+   */
+  protected void fireRestartedMessage() {
+    List<RunListener> list = cloneListenerList();
+    for (RunListener listener : list) {
+      listener.restarted();
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private List<RunListener> cloneListenerList() {
+    List<RunListener> list = null;
+    synchronized (runListeners) {
+      list = (List<RunListener>) runListeners.clone();
+    }
+    return list;
+  }
+
+  public void init() {
+    stop = false;
+    pause = false;
+    step = false;
+  }
+
+  /**
+   * Stops the execution of the scheduled events after the any events scheduled
+   * for the current tick have been executed.
+   */
+  public void stop() {
+    stop = true;
+  }
+
+  public void step() {
+    step = true;
+  }
+
+  public void setPause(boolean pause) {
+    this.pause = pause;
+  }
+
+  /**
+   * @return true if the runner should keep going
+   */
+  public boolean go() {
+    return !stop;
+  }
+
+  public void setController(Controller controller) {
+    this.controller = controller;
+  }
 }
