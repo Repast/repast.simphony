a9d9797326a0fe73847bf840d642f109d648f5d5
Jonathan Ozik <xio247@gmail.com>
2013-05-16 17:06:10 -0500

    Modified the default transition resolution strategy to use priority. Modified the UICreatorFactory to disable button of null statechart.

--- repast.simphony.statecharts.visualization/src/repast/simphony/statecharts/runtime/UICreatorFactory.java
+++ repast.simphony.statecharts.visualization/src/repast/simphony/statecharts/runtime/UICreatorFactory.java
@@ -32,103 +32,105 @@ import com.jgoodies.binding.PresentationModel;
  * @author Nick Collier
  */
 public class UICreatorFactory implements PPUICreatorFactory {
-  
- 
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see repast.simphony.ui.probe.PPUICreatorFactory#init(repast.simphony.ui.
-   * RSApplication)
-   */
-  @Override
-  public void init(RSApplication app) {
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see repast.simphony.ui.probe.PPUICreatorFactory#init(repast.simphony.ui.
+	 * RSApplication)
+	 */
+	@Override
+	public void init(RSApplication app) {
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * repast.simphony.ui.probe.PPUICreatorFactory#createUICreator(java.lang.Object
-   * , repast.simphony.ui.probe.FieldPropertyDescriptor)
-   */
-  @Override
-  public ProbedPropertyUICreator createUICreator(Object obj, FieldPropertyDescriptor fpd)
-      throws IllegalAccessException, IllegalArgumentException {
-    return new PPUICreator(obj, (DefaultStateChart<?>) fpd.getField().get(obj),
-        fpd.getDisplayName());
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * repast.simphony.ui.probe.PPUICreatorFactory#createUICreator(java.lang.Object
+	 * , repast.simphony.ui.probe.FieldPropertyDescriptor)
+	 */
+	@Override
+	public ProbedPropertyUICreator createUICreator(Object obj, FieldPropertyDescriptor fpd)
+			throws IllegalAccessException, IllegalArgumentException {
+		return new PPUICreator(obj, (DefaultStateChart<?>) fpd.getField().get(obj),
+				fpd.getDisplayName());
+	}
 
-  private static class PPUICreator extends DockableFrameAdapter implements ProbedPropertyUICreator, StatechartCloseListener,
-      DockableFrameListener {
+	private static class PPUICreator extends DockableFrameAdapter implements ProbedPropertyUICreator,
+			StatechartCloseListener, DockableFrameListener {
 
-    private DefaultStateChart<?> statechart;
-    private String name;
-    private Object obj;
-    private StateChartSVGDisplayController scsdc;
+		private DefaultStateChart<?> statechart;
+		private String name;
+		private Object obj;
+		private StateChartSVGDisplayController scsdc;
 
-    public PPUICreator(Object obj, DefaultStateChart<?> statechart, String name) {
-      this.obj = obj;
-      this.statechart = statechart;
-      this.name = name;
-    }
+		public PPUICreator(Object obj, DefaultStateChart<?> statechart, String name) {
+			this.obj = obj;
+			this.statechart = statechart;
+			this.name = name;
+		}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see repast.simphony.ui.probe.ProbedPropertyUICreator#getDisplayName()
-     */
-    @Override
-    public String getDisplayName() {
-      return name;
-    }
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see repast.simphony.ui.probe.ProbedPropertyUICreator#getDisplayName()
+		 */
+		@Override
+		public String getDisplayName() {
+			return name;
+		}
 
-    JButton button;
+		JButton button;
 
-    @Override
-    public JComponent getComponent(PresentationModel<Object> model) {
-      JPanel panel = new JPanel();
-      panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
-      button = new JButton("Display");
-      button.addActionListener(new ActionListener() {
+		@Override
+		public JComponent getComponent(PresentationModel<Object> model) {
+			JPanel panel = new JPanel();
+			panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+			button = new JButton("Display");
+			if (statechart == null) {
+				button.setEnabled(false);
+			} else {
+				button.addActionListener(new ActionListener() {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-          Object source = e.getSource();
-          if (source instanceof JButton) {
-            ((JButton) source).setEnabled(false);
-          }
-          scsdc = new StateChartSVGDisplayController(obj, statechart);
-          scsdc.registerCloseListener(PPUICreator.this);
-          RSApplication rsApp = RSApplication.getRSApplicationInstance();
-          if (rsApp != null) {
-            RSGui rsGui = rsApp.getGui();
-            if (rsGui != null) {
-              rsGui.addViewListener(PPUICreator.this);
-            }
-            
-          }
-          scsdc.createAndShowDisplay();
-        }
-      });
-      panel.add(button);
-      return panel;
-    }
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Object source = e.getSource();
+						if (source instanceof JButton) {
+							((JButton) source).setEnabled(false);
+						}
+						scsdc = new StateChartSVGDisplayController(obj, statechart);
+						scsdc.registerCloseListener(PPUICreator.this);
+						RSApplication rsApp = RSApplication.getRSApplicationInstance();
+						if (rsApp != null) {
+							RSGui rsGui = rsApp.getGui();
+							if (rsGui != null) {
+								rsGui.addViewListener(PPUICreator.this);
+							}
 
-    @Override
-    public void statechartClosed() {
-      button.setEnabled(true);
-    }
+						}
+						scsdc.createAndShowDisplay();
+					}
+				});
+			}
+			panel.add(button);
+			return panel;
+		}
 
-    @Override
-    public void dockableClosing(DockableFrameEvent evt) {
-      DockableFrame view = evt.getDockable();
-      Object closingObject = view.getClientProperty(ProbeManager.PROBED_OBJ_KEY);
-      if (closingObject == obj) {
-        if (scsdc != null)
-          scsdc.closeDisplayWithoutNotification();
-      }
+		@Override
+		public void statechartClosed() {
+			button.setEnabled(true);
+		}
 
-    }
-  }
+		@Override
+		public void dockableClosing(DockableFrameEvent evt) {
+			DockableFrame view = evt.getDockable();
+			Object closingObject = view.getClientProperty(ProbeManager.PROBED_OBJ_KEY);
+			if (closingObject == obj) {
+				if (scsdc != null)
+					scsdc.closeDisplayWithoutNotification();
+			}
+
+		}
+	}
 }
