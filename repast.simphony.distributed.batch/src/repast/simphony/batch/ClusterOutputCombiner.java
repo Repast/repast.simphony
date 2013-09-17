9763f6e24adf278ca8360e2f9592aa59da17dd9b
Nick Collier <srcnick@users.sourceforge.net>
2013-09-17 15:45:13 -0400

    updated to use new file pattern code for finding output files.

--- repast.simphony.distributed.batch/src/repast/simphony/batch/ClusterOutputCombiner.java
+++ repast.simphony.distributed.batch/src/repast/simphony/batch/ClusterOutputCombiner.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.PropertyConfigurator;
 
 import repast.simphony.batch.ssh.BaseOutputNamesFinder;
+import repast.simphony.batch.ssh.DefaultOutputPatternCreator;
 import repast.simphony.batch.ssh.LocalOutputFinder;
-import repast.simphony.batch.ssh.OutputAggregator;
+import repast.simphony.batch.ssh.MatchedFiles;
 import repast.simphony.batch.ssh.StatusException;
 import simphony.util.messages.MessageCenter;
 
@@ -32,15 +35,28 @@ public class ClusterOutputCombiner {
     props.load(new FileInputStream(in));
     PropertyConfigurator.configure(props);
   }
+  
+  private List<Pair<String, String>> createPatterns() throws IOException, XMLStreamException {
+    List<String> baseNames = new BaseOutputNamesFinder().find("./scenario.rs");
+    List<Pair<String, String>> filePatterns = new ArrayList<Pair<String, String>>();
+    for (String name : baseNames) {
+      DefaultOutputPatternCreator creator = new DefaultOutputPatternCreator(name);
+      // this has to be first, otherwise the non param map pattern will catch it.
+      filePatterns.add(Pair.of(creator.getFinalParamMapFileName(), creator.getParamMapPattern()));
+      filePatterns.add(Pair.of(creator.getFinalFileName(), creator.getFilePattern()));
+    }
+    return filePatterns;
+  }
 
   public void run() {
     try {
-      List<File> files = findOutput(workingDir);
-      OutputAggregator aggregator = new OutputAggregator();
+      List<MatchedFiles> files = findOutput(workingDir);
       new File(outputDir).mkdirs();
-      List<String> names = new BaseOutputNamesFinder().find("./scenario.rs");
-      aggregator.run(names, files, outputDir);
       msg.info("Aggregating output into " + outputDir);
+      for (MatchedFiles file : files) {
+        file.aggregateOutput(outputDir);
+      }
+      
     } catch (StatusException e) {
       e.printStackTrace();
     } catch (IOException ex) {
@@ -51,8 +67,12 @@ public class ClusterOutputCombiner {
     }
   }
 
-  public List<File> findOutput(String directory) throws StatusException {
+  public List<MatchedFiles> findOutput(String directory) throws StatusException, IOException, XMLStreamException {
+    List<Pair<String, String>> filePatterns = createPatterns();
     LocalOutputFinder finder = new LocalOutputFinder();
+    for (Pair<String, String> pattern : filePatterns) {
+      finder.addPattern(pattern.getLeft(), pattern.getRight());
+    }
     File localDir = new File(directory);
     msg.info(String.format("Finding output on localhost in %s", localDir.getPath()));
     return finder.run(localDir);
